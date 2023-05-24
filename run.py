537abe58a36a60b5e633125027114deea7478459
Gong Linxia <gonglx@sea.com>
2023-05-24 17:00:10 +0800

    add eval process

--- run.py
+++ run.py
@@ -2,23 +2,40 @@ import os
 import shutil
 from collections import defaultdict
 from time import gmtime, strftime
+from typing import Any
 
+import imageio
 import numpy as np
 import torch
-import tqdm
+import wandb
 import yaml
 from torch.optim.lr_scheduler import ExponentialLR
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from loss import Loss, NeRFLoss
 from nerf_render import NeRFRender, RayHelper
 from networks import NeRFFull
 
+to8b = lambda x: (255 * np.clip(x, 0, 1)).astype(np.uint8)
+
 
 class Trainer(object):
-    def __init__(self, nerf_render: NeRFRender, loss_fn: Loss, optimizer: torch.optim.Optimizer, scheduler,
-                 config: dict, H: int, W: int, focal: float) -> None:
+    def __init__(self,
+                 nerf_render: NeRFRender,
+                 loss_fn: Loss,
+                 optimizer: torch.optim.Optimizer,
+                 scheduler,
+                 config: dict,
+                 H: int,
+                 W: int,
+                 focal: float,
+                 log_dir: str,
+                 use_wandb: bool = False,
+                 save_best_ckpt_only: bool = True) -> None:
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.nerf_render = nerf_render.to(self.device)
+        self.model_name = nerf_render.__class__.__name__
         self.loss_fn = loss_fn
         self.optimizer = optimizer
         self.scheduler = scheduler
@@ -27,6 +44,10 @@ class Trainer(object):
         train_params = config["train_params"]
         self.num_rand = train_params["num_random"]
         self.num_epochs_focus_bbox = train_params["num_epochs_focus_bbox"] or 0
+        self.eval_interval = train_params["eval_interval"]
+        self.iter_cnt = 0  # current iteration step
+        self.start_epoch = 0
+        self.eval_loss = np.inf  # current evaluation loss
 
         self.ndc = config["dataset_params"]["ndc"]
         self._H = int(H)
@@ -37,6 +58,12 @@ class Trainer(object):
 
         # model setting
         self.use_viewdirs = config["model_params"]["use_viewdirs"]
+
+        # logging setting
+        self.use_wandb = use_wandb
+        self.log_dir = log_dir
+        self.save_best_ckpt_only = save_best_ckpt_only
+        self.fps = config["dataset_params"]["fps"] or 25
 
     def sample_rays_for_train(self, epoch: int):
         """Customized policy.
@@ -57,71 +84,141 @@ class Trainer(object):
         select_coords = coords[select_inds].long()  # (N_rand, 2)
         return select_coords
 
-    def train_one_epoch(self, epoch: int, dataset_train):
+    def train(self, num_epochs: int, dataloader_train: DataLoader, dataloader_val: DataLoader,
+              dataloader_test: DataLoader):
         H, W, focal = self._H, self._W, self._focal
         K = self._K
 
-        loss_accumulated = defaultdict(float)
-        self.nerf_render.train()
+        eval_loss = self.eval_loss  # np.inf
+        for epoch in range(self.start_epoch, num_epochs):
+            loss_accumulated = defaultdict(float)
+            self.nerf_render.train()
 
-        pbar = tqdm.tqdm(dataset_train)
-        pbar.set_description(f"Train #{epoch}")
+            pbar = tqdm(dataloader_train)
+            pbar.set_description(f"Train #{epoch}")
+            for i, data in enumerate(pbar):
+                image, pose = data["image"], data["pose"]
+                rays_o, rays_d = RayHelper.get_rays(H=W, W=W, K=K,
+                                                    c2w=torch.Tensor(pose[:3, :4]))  # (H, W, 3), (H, W, 3)
+                if self.ndc:
+                    rays_o, rays_d = RayHelper.ndc_rays(H=H, W=W, focal=focal, near=1., rays_o=rays_o, rays_d=rays_d)
+                # take num_rand rays for training
+                select_coords = self.sample_rays_for_train(epoch=epoch)
+                rays_o = rays_o[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
+                rays_d = rays_d[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
+                rgb_gt = torch.Tensor(image)[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
+
+                rgb, disp, acc, all_res = self.nerf_render.render(rays_o=rays_o.to(self.device),
+                                                                  rays_d=rays_d.to(self.device),
+                                                                  use_viewdirs=self.use_viewdirs,
+                                                                  white_bkgd=self.white_bkgd)
+                # zero the parameter gradients
+                self.optimizer.zero_grad()
+
+                # get loss
+                loss, loss_dict = self.loss_fn(pred=all_res, target={"rgb_map": rgb_gt})
+                loss.backward()
+
+                loss_accumulated["total"] += loss.item()
+                for k in loss_dict.keys():
+                    loss_accumulated[k] += loss_dict[k]
+
+                # step optimizer
+                self.optimizer.step()
+                self.scheduler.step()
+
+                # update logs
+                self.iter_cnt += 1
+                if (i % 50 == 0 and i > 0) or i == len(dataset_train) - 1:
+                    info = {k: v / (i + 1) for k, v in loss_accumulated.items()}
+                    tqdm.write(f"Train #{epoch} i={i}| {info}")
+                pbar.set_postfix({f"loss_{k}": v / (i + 1) for k, v in loss_accumulated.items()})
+
+                # # evaluate
+                if self.iter_cnt % self.eval_interval == 0:
+                    new_eval_loss = self.run_eval(dataloader_eval=dataloader_val)
+                    # # save model if metrics are better
+                    if new_eval_loss < eval_loss:
+                        self.save_model(epoch=epoch, eval_loss=new_eval_loss)
+                        eval_loss = new_eval_loss
+
+    @torch.no_grad()
+    def run_eval(self, dataloader_eval: DataLoader) -> float:
+        H, W, focal = self._H, self._W, self._focal
+        K = self._K
+
+        rgbs = []
+        disps = []
+        loss_accumulated = defaultdict(float)
+        self.nerf_render.eval()
+
+        pbar = tqdm(dataloader_eval)
+        pbar.set_description(f"Validation")
         for i, data in enumerate(pbar):
             image, pose = data["image"], data["pose"]
-            rays_o, rays_d = RayHelper.get_rays(H=W, W=W, K=K, c2w=torch.Tensor(pose[:3, :4]))  # (H, W, 3), (H, W, 3)
-            if self.ndc:
-                rays_o, rays_d = RayHelper.ndc_rays(H=H, W=W, focal=focal, near=1., rays_o=rays_o, rays_d=rays_d)
-            # take num_rand rays for training
-            select_coords = self.sample_rays_for_train(epoch=epoch)
-            rays_o = rays_o[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
-            rays_d = rays_d[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
-            rbg_gt = torch.Tensor(image)[select_coords[:, 0], select_coords[:, 1]]  # (N_rand, 3)
+            # render full image
+            rgb, disp, acc, all_res = self.nerf_render.render_full_image(H,
+                                                                         W,
+                                                                         focal,
+                                                                         K,
+                                                                         pose,
+                                                                         ndc=self.ndc,
+                                                                         use_viewdirs=self.use_viewdirs,
+                                                                         white_bkgd=self.white_bkgd)
+            rgb_gt = torch.Tensor(image).to(self.device)
 
-            rgb, disp, acc, all_res = self.nerf_render.render(rays_o=rays_o.to(self.device),
-                                                              rays_d=rays_d.to(self.device),
-                                                              use_viewdirs=self.use_viewdirs,
-                                                              white_bkgd=self.white_bkgd)
-            # zero the parameter gradients
-            self.optimizer.zero_grad()
+            loss, loss_dict = self.loss_fn(pred=all_res, target={"rgb_map": rgb_gt})
 
-            # get loss
-            loss, loss_dict = self.loss_fn(pred=all_res, target={"rgb_map": rbg_gt})
-            loss.backward()
-
+            # logging
+            rgbs.append(rgb.detach().cpu().numpy())
+            disps.append(disp.detach().cpu().numpy())
             loss_accumulated["total"] += loss.item()
             for k in loss_dict.keys():
                 loss_accumulated[k] += loss_dict[k]
+            info = {k: v / (i + 1) for k, v in loss_accumulated.items()}
+            tqdm.write(f"Validation| {info}")
+            if self.use_wandb:
+                for k, v in loss_accumulated.items():
+                    wandb.log({f"Validation/loss_{k}": v / (i + 1)}, step=self.iter_cnt)
 
-            # step optimizer
-            self.optimizer.step()
-            self.scheduler.step()
+        rgbs = np.stack(rgbs, 0)
+        disps = np.stack(disps, 0)
+        imageio.mimwrite(os.path.join(self.log_dir, f'{self.iter_cnt}_rgb.mp4'), to8b(rgbs), fps=self.fps, quality=8)
+        imageio.mimwrite(os.path.join(self.log_dir, f'{self.iter_cnt}_disp.mp4'),
+                         to8b(disps / np.max(disps)),
+                         fps=self.fps,
+                         quality=8)
+        print('write video done')
 
-            # update logs
-            if (i % 50 == 0 and i > 0) or i == len(dataset_train) - 1:
-                info = {k: v / (i + 1) for k, v in loss_accumulated.items()}
-                tqdm.tqdm.write(f"Train #{epoch} i={i}| {info}")
-            pbar.set_postfix({f"loss_{k}": v / (i + 1) for k, v in loss_accumulated.items()})
-        return loss_dict
+        return loss_accumulated["total"] / (i + 1)
 
-    def train(self, num_epochs: int, dataset_train, dataset_val, dataset_test):
-        eval_loss = np.inf
-        for epoch in range(num_epochs):
-            loss_dict = self.train_one_epoch(epoch=epoch, dataset_train=dataset_train)
-            # TODO logging
+    def save_model(self, epoch: int = None, eval_loss: Any = None):
+        """Save model to log_dir."""
+        if self.save_best_ckpt_only:
+            path_model = os.path.join(self.log_dir, f"{self.model_name}.pth")
+        else:
+            path_model = os.path.join(self.log_dir, f"{self.model_name}-{self.iter_cnt}.pth")
+        torch.save(
+            {
+                "iter_cnt": self.iter_cnt,
+                "epoch": epoch,
+                "loss": eval_loss,
+                "model_state_dict": self.nerf_render.network.state_dict(),
+                "optimizer_state_dict": self.optimizer.state_dict(),
+                "scheduler_state_dict": self.scheduler.state_dict()
+            },
+            path_model,
+        )
+        print(f"Model saved at : {path_model}")
 
-            # # evaluate
-            # new_eval_loss = self.run_eval()
-            # # save model if metrics are better
-            # if new_eval_loss < eval_loss:
-            #     self.save_model()
-            #     eval_loss = new_eval_loss
-
-    @torch.no_grad()
-    def run_eval(self, dataset_eval):
-        pass
-
-    def save_model(self):
-        pass
+    def load_model(self, model_path):
+        checkpoint = torch.load(model_path)
+        self.iter_cnt = int(checkpoint["iter_cnt"])
+        self.start_epoch = int(checkpoint["epoch"] or 0)
+        self.eval_loss = checkpoint["eval_loss"] or np.inf
+        self.nerf_render.network.load_state_dict(checkpoint['model_state_dict'])
+        self.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        self.scheduler.load_state_dict(checkpoint['scheduler_state_dict'])
 
 
 if __name__ == '__main__':
@@ -170,6 +267,9 @@ if __name__ == '__main__':
     H = int(dataset_train.H)
     W = int(dataset_train.W)
     focal = dataset_train.focal
+    dataloader_train = DataLoader(dataset_train, shuffle=True, batch_size=1, num_workers=0)
+    dataloader_eval = DataLoader(dataset_val, shuffle=True, batch_size=1, num_workers=0)
+    dataloader_test = DataLoader(dataset_test, shuffle=False, batch_size=1, num_workers=0)
 
     # ==== Train ====
 
@@ -194,9 +294,11 @@ if __name__ == '__main__':
                       config=config,
                       H=H,
                       W=W,
-                      focal=focal)
+                      focal=focal,
+                      log_dir=log_dir,
+                      save_best_ckpt_only=logging_params["save_best_ckpt_only"])
     if args.mode == "train":
         trainer.train(num_epochs=config["train_params"]["num_epochs"],
-                      dataset_train=dataset_train,
-                      dataset_val=dataset_val,
-                      dataset_test=dataset_test)
+                      dataloader_train=dataset_train,
+                      dataloader_val=dataset_val,
+                      dataloader_test=dataset_test)
